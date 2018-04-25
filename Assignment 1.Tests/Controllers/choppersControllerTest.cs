6e9030b3ade8bc9175f9bd49581df81dfb4ccd13
Arpiee <arpit.kohli01@gmail.com>
2018-04-25 01:06:40 -0400

    first commit

---
+++ Assignment 1.Tests/Controllers/choppersControllerTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Assignment_1.Controllers;
+
+namespace Assignment_1.Tests.Controllers
+{
+    /// <summary>
+    /// Summary description for choppersControllerTest
+    /// </summary>
+    [TestClass]
+    public class choppersControllerTest
+    {
+        [TestMethod]
+        public void IndexViewLoads()
+        {
+            // arrange
+            choppersController controller = new choppersController();
+
+            // act
+            var actual = controller.Index();
+
+            // assert
+            Assert.IsNotNull(actual);
+
+      
+        }
+    }
+}
