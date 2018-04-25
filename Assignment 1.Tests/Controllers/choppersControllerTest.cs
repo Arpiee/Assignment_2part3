63a810445e79fd222570444ba3d2168b852a6d67
Arpiee <arpit.kohli01@gmail.com>
2018-04-25 14:25:54 -0400

    second commit

--- Assignment 1.Tests/Controllers/choppersControllerTest.cs
+++ Assignment 1.Tests/Controllers/choppersControllerTest.cs
@@ -3,6 +3,10 @@ using System.Text;
 using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Assignment_1.Controllers;
+using Moq;
+using Assignment_1.Models;
+using System.Linq;
+using System.Web.Mvc;
 
 namespace Assignment_1.Tests.Controllers
 {
@@ -12,11 +16,39 @@ namespace Assignment_1.Tests.Controllers
     [TestClass]
     public class choppersControllerTest
     {
+        choppersController controller;
+        Mock<IMockchoppersRepository> mock;
+        List<chopper> choppers;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            // runs automatically before each unit test
+
+            // instantiate the mock object
+            mock = new Mock<IMockchoppersRepository>();
+
+            // instantiate the mock choppers  data
+            choppers = new List<chopper>
+            {
+                new chopper {chopperId = 1, Name = "chopper 1"},
+                new chopper {chopperId = 2, Name = "chopper 2"},
+                new chopper {chopperId = 3, Name = "chopper 3"}
+
+            };
+            // bind the date to the mock
+            mock.Setup(m => m.choppers).Returns(choppers.AsQueryable());
+
+            // initialize the controller and inject the dependency
+            controller = new choppersController(mock.Object);
+
+        }
+
+
         [TestMethod]
         public void IndexViewLoads()
         {
-            // arrange
-            choppersController controller = new choppersController();
+
 
             // act
             var actual = controller.Index();
@@ -24,7 +56,49 @@ namespace Assignment_1.Tests.Controllers
             // assert
             Assert.IsNotNull(actual);
 
-      
+
+        }
+        [TestMethod]
+
+        public void IndexLoadschoppers()
+        {
+            // act - cast ActionResult to ViewResult, then Model to list of choppers
+            var actual = (List<chopper>)((ViewResult)controller.Index()).Model;
+
+            // assert
+            CollectionAssert.AreEqual(choppers, actual);
+
+        }
+        [TestMethod]
+        public void DetailsValidchopperId()
+        {
+            //act
+            var actual = (chopper)((ViewResult)controller.Details(1)).Model;
+
+            //assert
+            Assert.AreEqual(choppers[0], actual);
+
+        }
+        [TestMethod]
+        public void DetailsInvalidchopperId()
+        {
+            // act - expect the Index view to load if no matching chopper
+            var actual = (ViewResult)controller.Details(4);
+
+            // assert
+            Assert.AreEqual("Error", actual.ViewName);
+
+        }
+        [TestMethod]
+
+        public void DetailsNochopperId()
+        {
+            // act
+            var actual = (ViewResult)controller.Details(null);
+
+            // assert
+            Assert.AreEqual("Error", actual.ViewName);
+
         }
     }
 }
